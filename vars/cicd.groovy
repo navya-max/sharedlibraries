eca459b939f468d72a9aa1d1c4f9efd8cd930586
navya <navyakatamgari123@gmail.com>
2025-07-21 14:22:48 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,15 @@
+def downloadcode(repo)
+{
+  git "https://github.com/IntelliqDevops/${repo}.git"
+}
+def buildartifact()
+{
+  sh "mvn package"
+}
+def deployment(jobname,ipaddress,contextpath)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}"
+}
+def runselenium(jobname)
+{
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
